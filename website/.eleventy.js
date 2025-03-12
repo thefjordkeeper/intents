1af70855d4e978c9b68549681d59fad8aa4b65ca
mks <slarkmodern@gmail.com>
2025-03-12 12:58:40 -0500

    New Backup on boot - 03/12/2025 - 12:58:25 PM

--- website/.eleventy.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("public/**/*");
-  return {
-    dir: {
-      input: "src-11ty",
-      output: "dist",
-    },
-  };
-};
